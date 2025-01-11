a350a8a2728ecaaade1b96944f8594d1a5411014
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-01-11 16:22:34 +0300

    Update ui_join_team.py

--- ui_join_team.py
+++ ui_join_team.py
@@ -10,16 +10,16 @@ class JoinTeamButton(Button):
 
     def __init__(self, team_number):
         self.team_number = team_number
-        super().__init__(label=f"Team {team_number + 1}")
+        super().__init__(label=f"Takım {team_number + 1}")
 
     async def callback(self, interaction: Interaction) -> Any:
         user = interaction.user
         print(self.team_number)
-        await interaction.user.send(f"You've joined team {self.team_number + 1}", delete_after=30)
+        await interaction.user.send(f"{self.team_number + 1} takıma katıldınız", delete_after=30)
         player = Player(user=interaction.user,
                         game=self.view.game,
                         team_number=self.team_number,
-                        info_message=await interaction.user.send("Connecting to the game..."))
+                        info_message=await interaction.user.send("Oyuna bağlanıyor..."))
         await player.update_info_message(player.questions[0][0][0])
         await self.view.game.update_info_message()
 
